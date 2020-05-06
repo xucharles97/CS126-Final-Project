8ddbf43df2e7a253ddef70e92b171dc31b5c5467
cyx2 <cyx2@illinois.edu>
2020-05-05 23:33:52 -0500

    Added PlayerBody class

---
+++ include/mylibrary/PlayerBody.h
@@ -0,0 +1,10 @@
+//
+// Created by School on 5/5/20.
+//
+
+#ifndef FINALPROJECT_PLAYERBODY_H
+#define FINALPROJECT_PLAYERBODY_H
+
+class PlayerBody {};
+
+#endif  // FINALPROJECT_PLAYERBODY_H
---
+++ src/PlayerBody.cpp
@@ -0,0 +1,5 @@
+//
+// Created by School on 5/5/20.
+//
+
+#include "mylibrary/PlayerBody.h"
