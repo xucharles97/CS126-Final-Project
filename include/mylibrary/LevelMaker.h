fb77df756f8a349414da9f57dd4c1acff6591b20
cyx2 <cyx2@illinois.edu>
2020-05-06 20:21:45 -0500

    Added LevelMaker class

---
+++ include/mylibrary/LevelMaker.h
@@ -0,0 +1,36 @@
+//
+// Created by School on 5/6/20.
+//
+
+#ifndef FINALPROJECT_LEVELMAKER_H
+#define FINALPROJECT_LEVELMAKER_H
+
+#include <Box2D/Common/b2Math.h>
+
+#include "GameWorld.h"
+
+class LevelMaker {
+ public:
+  LevelMaker();
+
+  enum GameState {ONGOING, GAME_OVER, FINISHED_LEVEL};
+
+  GameState update();
+  LevelMaker* getNextLevel();
+
+  virtual void LoadLevel();
+  virtual void draw();
+
+  void setNextLevel(LevelMaker &nextLevel);
+
+ private:
+  LevelMaker* nextLevel;
+  float bottomBoundary;
+  b2Vec2 spawnPoint;
+  b2Vec2 endPoint;
+  GameWorld level;
+
+
+};
+
+#endif  // FINALPROJECT_LEVELMAKER_H
---
+++ src/LevelMaker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by School on 5/6/20.
+//
+
+#include "mylibrary/LevelMaker.h"
